e1ca6ca0ac0eb98bb16f9c42144e0cf413b24420
rustamyagufarov <rustam@yagufarov.ru>
2025-02-25 00:43:55 +0200

    Сумма чисел [#505208]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,8 +7,7 @@ class CounterTest {
 
     @Test
     void when0And10Then55() {
-        int start = 0;
-        int finish = 10;
+        int start = 0, finish = 10;
         int expected = 55;
         int output = Counter.sum(start,finish);
         assertThat(output).isEqualTo(expected);
@@ -16,28 +15,25 @@ class CounterTest {
 
     @Test
     void when3And8Then33() {
-        int start = 3;
-        int finish = 8;
+        int start = 3, finish = 8;
         int expected = 33;
-        int output = Counter.sum(start,finish);
+        int output = Counter.sum(start, finish);
         assertThat(output).isEqualTo(expected);
     }
 
     @Test
     void when1And1Then1() {
-        int start = 1;
-        int finish = 1;
+        int start = 1, finish = 1;
         int expected = 1;
-        int output = Counter.sum(start,finish);
+        int output = Counter.sum(start, finish);
         assertThat(output).isEqualTo(expected);
     }
 
     @Test
     void when10And3Then1() {
-        int start = 10;
-        int finish = 3;
+        int start = 10, finish = 3;
         int expected = 0;
-        int output = Counter.sum(start,finish);
+        int output = Counter.sum(start, finish);
         assertThat(output).isEqualTo(expected);
     }
 }
