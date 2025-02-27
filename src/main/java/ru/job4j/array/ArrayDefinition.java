9aa54cce010fa9611a31eb5ebab6ae04701f97b0
rustamyagufarov <rustam@yagufarov.ru>
2025-02-28 00:27:54 +0200

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,6 +6,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Alexey Esipov";
+        names[2] = "Andrei Hincu";
+        names[3] = "Stas Korobeinikov";
+
+        for (byte i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
 
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
